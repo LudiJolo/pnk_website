8f633b558d2d78b15e59304f9d64a1ee6ca71958
LudiJolo <joloh18@yahoo.com>
2023-05-19 10:36:09 -0700

    Implemented Navigation
    - created navbar
    - implemented routing to different pages
    - home, about, shop, request, contact

---
+++ pnk_website/src/components/navbar/Navi.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
+import {Outlet} from "react-router-dom";
+function Navi() {
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">PNK</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">About PNK</Nav.Link>
+              <Nav.Link href="/shop">Shop</Nav.Link>
+              <Nav.Link href="/request">Request a Mod</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet/>
+    </>
+  );
+}
+
+export default Navi;
---
+++ pnk_website/src/components/shop/Shop.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Shop() {
+  return (
+    <div>
+        <h1>Shop page</h1>
+        <h1>Shop page</h1>
+        <h1>Shop page</h1>
+        <h1>Shop page</h1>
+    </div>
+  );
+}
+
+export default Shop;
