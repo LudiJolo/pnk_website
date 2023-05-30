3c22171fb424a01231bb0915caba82a561d676e3
LudiJolo <joloh18@yahoo.com>
2023-05-30 15:41:25 -0700

    keyboard.js
    - installed icons
    - image gallery
    - changed keyboard page layout

--- pnk_website/src/components/collection/Keyboard.js
+++ pnk_website/src/components/collection/Keyboard.js
@@ -1,63 +1,75 @@
 import React from "react";
-import {Tabs, Tab} from "react-bootstrap"
+import { Tabs, Tab } from "react-bootstrap";
+import * as Icons from "react-bootstrap-icons";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
 import sampleimg from "./img/sample.png";
+import showcase1 from "./img/showcase1.png";
+import showcase2 from "./img/showcase2.png";
+import showcase3 from "./img/showcase3.png";
+
+const images = [
+  {
+    original: showcase1,
+    thumbnail: showcase1,
+  },
+  {
+    original: showcase2,
+    thumbnail: showcase2,
+  },
+  {
+    original: showcase3,
+    thumbnail: showcase3,
+  },
+];
 
 function Keyboard() {
   return (
-    <div class="detailcontainer p-5">
-      <div class="detailinfo row pt-4 mb-5">
-        <div class="col-md-5">
-          <img class="img-fluid p-3" src={sampleimg} />
+    <div class="detailcontainer p-md-5">
+      <h1 align="center" class="p-5">
+        Alloy FPS Origins
+      </h1>
+      <div class="detailinfo pt-4 mb-5">
+        <ImageGallery items={images} autoPlay={true} />
+      </div>
+      <div class="row  text-center">
+        <div class="col-md-4 mx-auto py-3">
+          <div class="card shadow-lg py-2">
+            <div class="card-body">
+              <Icons.Tools className="icon" />
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item py-4 fs-5">HyperX keycaps</li>
+                <li class="list-group-item py-4 fs-5">Foam installed</li>
+                <li class="list-group-item py-4 fs-5">O-rings</li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <div class="col-md-7">
-          <h1>Alloy FPS Origins</h1>
-          <table class="table table-striped">
-            <tbody>
-              <tr scope="row">
-                <th>Brand</th>
-                <td>HyperX</td>
-              </tr>
-              <tr scope="row">
-                <th>Size</th>
-                <td>Full size</td>
-              </tr>
-              <tr scope="row">
-                <th>Switch</th>
-                <td>Cherry MX red</td>
-              </tr>
-              <tr scope="row">
-                <th>Keycaps</th>
-                <td>Generic HyperX Keycaps</td>
-              </tr>
-              <tr scope="row">
-                <th>Color theme</th>
-                <td>All black</td>
-              </tr>
-            </tbody>
-          </table>
-          <div class="py-3">
-            <span>Do you want this specific mod? </span>
-            <button type="button" class="btn btn-primary">
-              Order preset
-            </button>
+        <div class="col-md-4 mx-auto py-3">
+          <div class="card shadow-lg py-2">
+            <div class="card-body">
+              <Icons.KeyboardFill className="icon" />
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item py-4 fs-5">by HyperX</li>
+                <li class="list-group-item py-4 fs-5">Full size</li>
+                <li class="list-group-item py-4 fs-5">Cherry MX Reds</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mx-auto py-3">
+          <div class="card shadow-lg py-2">
+            <div class="card-body">
+              <Icons.PaletteFill className="icon" />
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item py-4 fs-5">No reference</li>
+                <li class="list-group-item py-4 fs-5">All dark</li>
+                <li class="list-group-item py-4 fs-5">RGB lighting</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-      <Tabs
-      defaultActiveKey="profile"
-      id="uncontrolled-tab-example"
-      className="mb-3"
-    >
-      <Tab eventKey="overview" title="Overview">
-        Overview
-      </Tab>
-      <Tab eventKey="features" title="Features">
-        Features
-      </Tab>
-      <Tab eventKey="sound" title="Sound Test">
-        Video
-      </Tab>
-    </Tabs>
     </div>
   );
 }
