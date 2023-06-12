75ff13081efb8a70501dcc53ae384e73f99a6e06
LudiJolo <joloh18@yahoo.com>
2023-06-12 13:56:04 -0700

    designed contact.js page

--- pnk_website/src/components/contact/Contact.js
+++ pnk_website/src/components/contact/Contact.js
@@ -1,26 +1,50 @@
-import {React, useReducer} from "react";
+import { React, useReducer } from "react";
 import emailjs from "@emailjs/browser";
+import "./contact.css";
 
 function Contact() {
   return (
-    <form class="mx-5 px-5">
-      <div class="form-group">
-        <label for="email">Email Address</label>
-        <input type="email" class="form-control" id="email" placeholder="Email"/>
-      </div>
-      <div class="form-group">
-        <label for="name">Your Name</label>
-        <input type="text" class="form-control" id="name" placeholder="Full name"/>
-      </div>
-      <div class="form-group">
-        <label for="phone">Phone Number</label>
-        <input type="tel" class="form-control" id="phone" placeholder="(###)-###-####"/>
-      </div>
-      <div class="form-group">
-        <label for="textbox">Message</label>
-        <textarea class="form-control" id="textbox" rows="3" placeholder="Let me know your thoughts"></textarea>
-      </div>
-    </form>
+    <div class="contactcontainer">
+      <form class="contactform shadow-lg mx-auto">
+        <h1 align="center">Reach out to me</h1>
+        <div class="form-group mb-3">
+          <label for="email"><b>Email Address</b></label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            placeholder="Email"
+          />
+        </div>
+        <div class="form-group mb-3">
+          <label for="name"><b>Your Name</b></label>
+          <input
+            type="text"
+            class="form-control"
+            id="name"
+            placeholder="Full name"
+          />
+        </div>
+        <div class="form-group mb-3">
+          <label for="phone"><b>Phone Number</b></label>
+          <input
+            type="tel"
+            class="form-control"
+            id="phone"
+            placeholder="(###)-###-####"
+          />
+        </div>
+        <div class="form-group ">
+          <label for="textbox"><b>Message</b></label>
+          <textarea
+            class="form-control"
+            id="textbox"
+            rows="3"
+            placeholder="Let me know your thoughts"
+          ></textarea>
+        </div>
+      </form>
+    </div>
   );
 }
 
