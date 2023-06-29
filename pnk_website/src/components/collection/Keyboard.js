055f17b398e72379027dc79fdab94a5f95675d4f
LudiJolo <joloh18@yahoo.com>
2023-06-29 11:31:14 -0700

    Fetch firebase using Collection.js
    - collection page now fetch data from firebase
    - uses id in query string to pass selected keyboard

--- pnk_website/src/components/collection/Keyboard.js
+++ pnk_website/src/components/collection/Keyboard.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
 import { Tabs, Tab } from "react-bootstrap";
 import * as Icons from "react-bootstrap-icons";
 import ImageGallery from "react-image-gallery";
@@ -8,6 +10,8 @@ import sampleimg from "./img/sample.png";
 import showcase1 from "./img/showcase1.png";
 import showcase2 from "./img/showcase2.png";
 import showcase3 from "./img/showcase3.png";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase/firebase-config";
 
 const images = [
   {
@@ -25,74 +29,98 @@ const images = [
 ];
 
 function Keyboard() {
+  const [keyboard, setKeyboard] = useState(null);
+  const { keyboard_id } = useParams();
+  console.log(keyboard_id);
+  useEffect(() => {
+    (async () => {
+      try {
+        const item = doc(db, "keyboards", keyboard_id);
+        const itemSnap = await getDoc(item);
+        if (itemSnap.exists()) {
+          console.log("Document data:", itemSnap.data());
+          setKeyboard(itemSnap.data());
+        } else {
+          console.log("No such document!");
+        }
+      } catch (err) {
+        console.log("Error occured when fetching keyboards");
+      }
+    })();
+  }, []);
+
   return (
     <div class="detailcontainer p-md-3">
-      <div class="jumbotron">
-        <div>
-          <h1>Alloy FPS Origins</h1>
-          <p>
-            Do you want this custom keyboard?{" "}
-            <a class="btn btn-primary scrollto">Buy preset</a>
-          </p>
-          <a class="btn btn-primary scrollto" href="#soundtest">
-            Sound test
-          </a>
-        </div>
-      </div>
+      {keyboard && (
+        <>
+          <div class="jumbotron">
+            <div>
+              <h1>{keyboard.name}</h1>
+              <p>
+                Do you want this custom keyboard?{" "}
+                <a class="btn btn-primary scrollto">Buy preset</a>
+              </p>
+              <a class="btn btn-primary scrollto" href="#soundtest">
+                Sound test
+              </a>
+            </div>
+          </div>
 
-      <div class="detailinfo pt-4 mb-5">
-        <ImageGallery items={images} autoPlay={true} />
-      </div>
-      <div class="row  text-center">
-        <div class=" col-md-4 mx-auto py-3">
-          <div class="info1 card shadow-lg py-2">
-            <div class="card-body">
-              <Icons.Tools className="icon" />
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item py-4 fs-5">HyperX keycaps</li>
-                <li class="list-group-item py-4 fs-5">Foam installed</li>
-                <li class="list-group-item py-4 fs-5">O-rings</li>
-              </ul>
+          <div class="detailinfo pt-4 mb-5">
+            <ImageGallery items={images} autoPlay={true} />
+          </div>
+          <div class="row  text-center">
+            <div class=" col-md-4 mx-auto py-3">
+              <div class="info1 card shadow-lg py-2">
+                <div class="card-body">
+                  <Icons.Tools className="icon" />
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item py-4 fs-5">{keyboard.specifics.spec1}</li>
+                    <li class="list-group-item py-4 fs-5">{keyboard.specifics.spec2}</li>
+                    <li class="list-group-item py-4 fs-5">{keyboard.specifics.spec3}</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class=" col-md-4 mx-auto py-3">
+              <div class="info2 card shadow-lg py-2">
+                <div class="card-body">
+                  <Icons.KeyboardFill className="icon" />
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item py-4 fs-5">{keyboard.general.brand}</li>
+                    <li class="list-group-item py-4 fs-5">{keyboard.general.size}</li>
+                    <li class="list-group-item py-4 fs-5">{keyboard.general.switch}</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class=" col-md-4 mx-auto py-3">
+              <div class="info3 card shadow-lg py-2">
+                <div class="card-body">
+                  <Icons.PaletteFill className="icon" />
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item py-4 fs-5">{keyboard.theme.reference}</li>
+                    <li class="list-group-item py-4 fs-5">{keyboard.theme.colorTheme}</li>
+                    <li class="list-group-item py-4 fs-5">{keyboard.theme.otherInfo}</li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div class=" col-md-4 mx-auto py-3">
-          <div class="info2 card shadow-lg py-2">
-            <div class="card-body">
-              <Icons.KeyboardFill className="icon" />
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item py-4 fs-5">by HyperX</li>
-                <li class="list-group-item py-4 fs-5">Full size</li>
-                <li class="list-group-item py-4 fs-5">Cherry MX Reds</li>
-              </ul>
-            </div>
+          <div
+            id="soundtest"
+            align="center"
+            class="mt-5 py-5 text-bg-dark shadow-lg"
+          >
+            <h1>Sound Test</h1>
+            <ReactPlayer
+              id="player"
+              url={keyboard.soundTest}
+              controls={true}
+            />
           </div>
-        </div>
-        <div class=" col-md-4 mx-auto py-3">
-          <div class="info3 card shadow-lg py-2">
-            <div class="card-body">
-              <Icons.PaletteFill className="icon" />
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item py-4 fs-5">No reference</li>
-                <li class="list-group-item py-4 fs-5">All dark</li>
-                <li class="list-group-item py-4 fs-5">RGB lighting</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div
-        id="soundtest"
-        align="center"
-        class="mt-5 py-5 text-bg-dark shadow-lg"
-      >
-        <h1>Sound Test</h1>
-        <ReactPlayer
-          id="player"
-          url={"https://www.youtube.com/watch?v=v2yJa9g5Ib8"}
-          controls={true}
-        />
-      </div>
+        </>
+      )}
     </div>
   );
 }
