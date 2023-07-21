0715ad1e0d74f73d671906b269e8e2d185c9afd3
LudiJolo <joloh18@yahoo.com>
2023-07-21 14:31:55 -0700

    Update editKeyboard.js
    - edits are now applied
    - pressing edit shows existing fields

--- pnk_website/src/components/admin/editKeyboard.js
+++ pnk_website/src/components/admin/editKeyboard.js
@@ -2,11 +2,37 @@ import React from "react";
 import { useRef, useState, useEffect } from "react";
 import { Modal, Button, Form, Row, Col } from "react-bootstrap";
 import { db } from "../firebase/firebase-config";
-import { doc, setDoc, addDoc, collection, updateDoc, getDoc } from "firebase/firestore";
+import {
+  doc,
+  setDoc,
+  addDoc,
+  collection,
+  updateDoc,
+  getDoc,
+} from "firebase/firestore";
 import { getStorage, ref, getDownloadURL, uploadBytes } from "firebase/storage";
 const EditKeyboard = (props) => {
   const [keebData, setKeeb] = useState(null);
-  const [nameRef, setName] = useState(null);
+
+  
+  const fetchDoc = async () => {
+    try {
+      if (props.show) {
+        const docRef = doc(db, "keyboards", props.keebId);
+        const docSnap = await getDoc(docRef);
+
+        setKeeb(docSnap.data());
+        console.log("Document data:", docSnap.data());
+      }
+    } catch (err) {
+      console.log("Error occured when fetching keyboard data");
+    }
+  };
+  useEffect(() => {
+    fetchDoc();
+  }, [props.show]);
+
+  const nameRef = useRef(null);
   const descRef = useRef(null);
   const soundRef = useRef(null);
   const brandRef = useRef(null);
@@ -21,24 +47,6 @@ const EditKeyboard = (props) => {
   const [img1, setimg1] = useState(null);
   const [img2, setimg2] = useState(null);
   const [img3, setimg3] = useState(null);
-  const fetchDoc = async () => {
-    try {
-      if (props.show) {
-        const docRef = doc(db, "keyboards", props.keebId);
-        const docSnap = await getDoc(docRef);
-
-        setKeeb(docSnap.data());
-        console.log("Document data:", docSnap.data());
-        setName(docSnap.data().name);
-      }
-    } catch (err) {
-      console.log("Error occured when fetching keyboard data");
-    }
-  };
-  useEffect(() => {
-    fetchDoc();
-  }, [props.show]);
-
 
   const handleImg1 = (e) => {
     const file = e.target.files[0];
@@ -56,59 +64,78 @@ const EditKeyboard = (props) => {
   const submitEditHandler = (e) => {
     e.preventDefault();
     const storage = getStorage();
-    const keebRef = doc(db, 'keyboards', props.keebId);
-    const imageUploadHandler = async() =>{
-      if(img1){
+    const keebRef = doc(db, "keyboards", props.keebId);
+    const imageUploadHandler = async () => {
+      if (img1) {
         const storageRef1 = ref(storage, img1.name);
         const uploadTask1 = uploadBytes(storageRef1, img1);
         const downloadURL1 = await getDownloadURL((await uploadTask1).ref);
         await updateDoc(keebRef, {
           imgURL1: downloadURL1,
-        })
+        });
       }
-      if(img2){
+      if (img2) {
         const storageRef2 = ref(storage, img2.name);
         const uploadTask2 = uploadBytes(storageRef2, img2);
         const downloadURL2 = await getDownloadURL((await uploadTask2).ref);
         await updateDoc(keebRef, {
           imgURL1: downloadURL2,
-        })
+        });
       }
-      if(img3){
+      if (img3) {
         const storageRef3 = ref(storage, img3.name);
         const uploadTask3 = uploadBytes(storageRef3, img3);
         const downloadURL3 = await getDownloadURL((await uploadTask3).ref);
         await updateDoc(keebRef, {
           imgURL3: downloadURL3,
-        })
-      }
-      const textInputHandler = async() =>{
-        const editedKeyboard = await updateDoc(keebRef, {
-          name: nameRef.current.value,
-          desc: descRef.current.value,
-          soundTest: soundRef.current.value,
-          general: {
-            brand: brandRef.current.value,
-            size: sizeRef.current.value,
-            switch: switchRef.current.value,
-          },
-          specifics: {
-            spec1: spec1Ref.current.value,
-            spec2: spec2Ref.current.value,
-            spec3: spec3Ref.current.value,
-          },
-          theme: {
-            reference: referRef.current.value,
-            colorTheme: colorRef.current.value,
-            otherInfo: otherRef.current.value,
-          },
         });
-      };
-      imageUploadHandler();
-      textInputHandler();
-      props.onHide();
+      }
     };
-
+    const textInputHandler = async () => {
+      const editedKeyboard = await updateDoc(keebRef, {
+        name: nameRef.current.value ? nameRef.current.value : keebData.name,
+        desc: descRef.current.value ? descRef.current.value : keebData.desc,
+        soundTest: soundRef.current.value
+          ? soundRef.current.value
+          : keebData.soundTest,
+        general: {
+          brand: brandRef.current.value
+            ? brandRef.current.value
+            : keebData.general.brand,
+          size: sizeRef.current.value
+            ? sizeRef.current.value
+            : keebData.general.size,
+          switch: switchRef.current.value
+            ? switchRef.current.value
+            : keebData.general.switch,
+        },
+        specifics: {
+          spec1: spec1Ref.current.value
+            ? spec1Ref.current.value
+            : keebData.specifics.spec1,
+          spec2: spec2Ref.current.value
+            ? spec2Ref.current.value
+            : keebData.specifics.spec2,
+          spec3: spec3Ref.current.value
+            ? spec3Ref.current.value
+            : keebData.specifics.spec3,
+        },
+        theme: {
+          reference: referRef.current.value
+            ? referRef.current.value
+            : keebData.theme.reference,
+          colorTheme: colorRef.current.value
+            ? colorRef.current.value
+            : keebData.theme.colorTheme,
+          otherInfo: otherRef.current.value
+            ? otherRef.current.value
+            : keebData.theme.otherInfo,
+        },
+      });
+    };
+    imageUploadHandler();
+    textInputHandler();
+    props.onHide();
   };
   return (
     <Modal
@@ -130,11 +157,8 @@ const EditKeyboard = (props) => {
                 <Form.Label>Name</Form.Label>
                 <Form.Control
                   type="text"
-                  placeholder=""
+                  placeholder={keebData.name}
                   ref={nameRef}
-                  value={keebData.name}
-                  onChange={(e)=>(setName(e.target.value))}
-                  required
                 />
                 <Form.Text className="text-muted">
                   example: HyperX Alloy Origins
@@ -145,11 +169,9 @@ const EditKeyboard = (props) => {
                 <Form.Control
                   as="textarea"
                   rows={3}
-                  placeholder=""
+                  placeholder={keebData.desc}
                   name="desc"
                   ref={descRef}
-                  value={keebData.desc}
-                  required
                 />
                 <Form.Text className="text-muted">
                   Briefly summarize what this keyboard is about
@@ -159,11 +181,9 @@ const EditKeyboard = (props) => {
                 <Form.Label>Sound test</Form.Label>
                 <Form.Control
                   type="text"
-                  placeholder=""
+                  placeholder={keebData.soundTest}
                   name="sound_url"
                   ref={soundRef}
-                  value={keebData.soundTest}
-                  required
                 />
                 <Form.Text className="text-muted">Enter YouTube link</Form.Text>
               </Form.Group>
@@ -173,76 +193,59 @@ const EditKeyboard = (props) => {
                   <Form.Label>General</Form.Label>
                   <Form.Control
                     type="text"
-                    placeholder="Brand"
+                    placeholder={keebData.general.brand}
                     ref={brandRef}
-                    value={keebData.general.brand}
-                    required
                   />
                   <Form.Control
                     type="text"
-                    placeholder="Size"
+                    placeholder={keebData.general.size}
                     ref={sizeRef}
-                    value={keebData.general.size}
-                    required
                   />
                   <Form.Control
                     type="text"
-                    placeholder="Switch type"
+                    placeholder={keebData.general.switch}
                     ref={switchRef}
-                    value={keebData.general.switch}
-                    required
                   />
                 </Col>
                 <Col md={4}>
                   <Form.Label>Specs</Form.Label>
                   <Form.Control
                     type="text"
-                    placeholder="Spec#1"
+                    placeholder={keebData.specifics.spec1}
                     ref={spec1Ref}
-                    value={keebData.specifics.spec1}
-                    required
                   />
                   <Form.Control
                     type="text"
-                    placeholder="Spec#2"
+                    placeholder={keebData.specifics.spec2}
                     ref={spec2Ref}
-                    value={keebData.specifics.spec2}
-                    required
                   />
                   <Form.Control
                     type="text"
-                    placeholder="Spec#3"
+                    placeholder={keebData.specifics.spec3}
                     ref={spec3Ref}
-                    value={keebData.specifics.spec3}
-                    required
                   />
                 </Col>
                 <Col md={4}>
                   <Form.Label>Color Theme</Form.Label>
                   <Form.Control
                     type="text"
-                    placeholder="Reference"
+                    placeholder={keebData.theme.colorTheme}
                     ref={referRef}
-                    value={keebData.theme.colorTheme}
-                    required
                   />
                   <Form.Control
                     type="text"
-                    placeholder="Colors"
+                    placeholder={keebData.theme.reference}
                     ref={colorRef}
-                    value={keebData.theme.reference}
-                    required
                   />
                   <Form.Control
                     type="text"
-                    placeholder="Other Info"
+                    placeholder={keebData.theme.otherInfo}
                     ref={otherRef}
-                    value={keebData.theme.otherInfo}
-                    required
                   />
                 </Col>
                 <Col className="pt-3">
-                  <Form.Label>Images</Form.Label><br/>
+                  <Form.Label>Images</Form.Label>
+                  <br />
                   <Form.Label>Top view</Form.Label>
                   <input
                     type="file"
