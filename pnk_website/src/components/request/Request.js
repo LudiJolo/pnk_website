9e808adc2b3ce42d1d22a67c8a2b1308110b59d7
LudiJolo <joloh18@yahoo.com>
2023-08-24 15:44:27 -0700

    Updated request page

--- pnk_website/src/components/request/Request.js
+++ pnk_website/src/components/request/Request.js
@@ -1,16 +1,78 @@
 import React from "react";
-
-const Request = () =>{
-    return <>
-    <div class="jumbotron bg-secondary">
-            <div>
-              <h1> Webpage not available</h1>
-              <p>
-                Currently under construction
-              </p>
-            </div>
+import "./request.css";
+import Lottie from "lottie-react";
+import form from "./animations/animation_llpofrva.json";
+import phone from "./animations/animation_llpogmbh.json";
+import payment from "./animations/animation_llpqaxni.json";
+import { Button } from "react-bootstrap";
+const Request = () => {
+  const anim_style = {
+    height: 125,
+  };
+  return (
+    <div class="requestcontainer">
+      <h1 align="center">Custom Requests</h1>
+      <div align="center" class="container ">
+        <div class="card request-desc">
+          <div class="card-body">
+            Dreaming of your own unique keyboard? PNK offers custom requests to
+            provide personalized keyboards based on the customer's aesthetic
+            taste. You'll have the opportunity to connect with Albert to discuss
+            your desired keyboard design down to its specific detail. Please
+            fill out the request form, so we can begin processing your own
+            custom keyboard.
           </div>
-    </>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+              Note: We will also post your finalized keyboard in the Collections
+              tab.
+            </li>
+          </ul>
+        </div>
+        <br />
+        <Button
+          href="https://docs.google.com/forms/d/e/1FAIpQLSf_HLUvlvUCc6lyAq1O_VIF34DbZ-xeAupn2t7b4a7INpWXdQ/viewform"
+          className="mx-auto btn-success"
+        >
+          Request form
+        </Button>
+        <br />
+        <br />
+        <div class="container">
+          <h1 align="center">How it works:</h1>
+          <div class="request-steps row ">
+            <div
+              align="center"
+              class="col-md-9 d-flex justify-content-center align-items-center"
+            >
+              Fill-out and submit a google form
+            </div>
+            <Lottie class="col-md-3" animationData={form} style={anim_style} />
+            <div
+              align="center"
+              class="col-md-9 d-flex justify-content-center align-items-center"
+            >
+              Albert will respond via email to discuss specific keyboard
+              requests
+            </div>
+            <Lottie class="col-md-3" animationData={phone} style={anim_style} />
+            <div
+              align="center"
+              class="col-md-9 d-flex justify-content-center align-items-center"
+            >
+              Pay using your desired payment method once the keyboard is
+              finished
+            </div>
+            <Lottie
+              class="col-md-3"
+              animationData={payment}
+              style={anim_style}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Request;
